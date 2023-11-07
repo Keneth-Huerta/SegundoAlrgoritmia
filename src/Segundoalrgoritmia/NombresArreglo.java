d3726ec1e55e65c45ca3a544c0da67328bfe1ad2
Keneth-Huerta <kenethissac@gmail.com>
2023-11-07 12:25:53 -0600

    promedio

--- src/Segundoalrgoritmia/NombresArreglo.java
+++ src/Segundoalrgoritmia/NombresArreglo.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class NombresArreglo {
 
     public static void main(String[] args) {
-        String[] nombres = new String[5];
+        String[] alumnos = new String[5];
+        float[] promedios = new float[5];
+        float promedio = 0;
         Scanner in = new Scanner(System.in);
+        Scanner inR = new Scanner(System.in);
 
-        for (int i = 0; i < nombres.length; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.print(i + 1 + ".-");
-            nombres[i] = in.nextLine();
-            
+            alumnos[i] = in.nextLine();
+            System.out.print("promedio: ");
+            promedios[i] = inR.nextFloat();
+
         }
+        for (int i = 0; i < 5; i++) {
+            promedio += promedios[i];
+        }
+        promedio /= promedios.length;
+        System.out.println("\nel promedio es: " + promedio);
     }
 }
