6d3563f5af2c3be94a3ee6235d723c896c2219b1
Keneth-Huerta <kenethissac@gmail.com>
2023-11-07 11:44:04 -0600

    nombresArreglo

---
+++ src/Segundoalrgoritmia/NombresArreglo.java
@@ -0,0 +1,17 @@
+package Segundoalrgoritmia;
+
+import java.util.Scanner;
+
+public class NombresArreglo {
+
+    public static void main(String[] args) {
+        String[] nombres = new String[5];
+        Scanner in = new Scanner(System.in);
+
+        for (int i = 0; i < nombres.length; i++) {
+            System.out.print(i + 1 + ".-");
+            nombres[i] = in.nextLine();
+
+        }
+    }
+}
---
+++ src/Segundoalrgoritmia/Vectores.java
@@ -0,0 +1,8 @@
+package Segundoalrgoritmia;
+
+public class Vectores {
+     public static void main(String[] args) {
+        
+        
+     }
+}
