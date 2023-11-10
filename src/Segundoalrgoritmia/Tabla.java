e6cdb00aad60671ca684d228575f4a94f8412df2
Keneth-Huerta~ <kenethissac@gmail.com>
2023-11-10 12:23:05 -0600

    tabla

---
+++ src/Segundoalrgoritmia/Tabla.java
@@ -0,0 +1,16 @@
+package Segundoalrgoritmia;
+
+import java.util.Scanner;
+
+public class Tabla {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num[] = new int[10];
+        System.out.println("Dame un número");
+        for (int i = 0; i < 9; i++) {
+            
+        }
+
+    }
+}
