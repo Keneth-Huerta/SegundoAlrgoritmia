9f4c9cdc87379a7637e568690a49bdaf50619c90
Keneth-Huerta <kenethissac@gmail.com>
2023-12-05 11:26:58 -0600

    algo

--- src/Segundoalrgoritmia/DosDimensiones.java
+++ src/Segundoalrgoritmia/DosDimensiones.java
@@ -1,15 +1,20 @@
+package Segundoalrgoritmia;
+
 public class DosDimensiones {
-public static void main(String[] args) {
-int filas, columnas;
-String libros[] [] - {
-{"Ana Karenina", "Cien Afos de Soledad", "Orgullo y Prejuicio", "Mujercitas", "El diablo de los numeros"},
-{"Leon Toltoi", "Gabriel Garcia Marquez", "Jane Austen", "Louisa May Alcott", "Hans Magnus"}, );
-System.out.printin ("Titulo "+"\t \t"+"Autor");
-for (filas = 0; filas < 2; filas++) {
-System.out.println (x: "") ;
-for (columnas -0; columnas<5; columnas++ ) {
-System. out.print (libros [filas] [columnas]+"\t") ;
-System.out.println (x: "");
-1
-}
+    public static void main(String[] args) {
+        int filas, columnas;
+        String libros[][] = {
+                { "Ana Karenina", "Cien Afos de Soledad", "Orgullo y Prejuicio", "Mujercitas",
+                        "El diablo de los numeros" },
+                { "Leon Toltoi", "Gabriel Garcia Marquez", "Jane Austen", "Louisa May Alcott", "Hans Magnus" }, };
+        System.out.println("Titulo " + "\t \t" + "Autor");
+        for (filas = 0; filas < 2; filas++) {
+            System.out.println("");
+            for (columnas = 0; columnas < 5; columnas++) {
+                System.out.print(libros[filas][columnas] + "\t");
+            }
+            System.out.println("");
+
+        }
+    }
 }
