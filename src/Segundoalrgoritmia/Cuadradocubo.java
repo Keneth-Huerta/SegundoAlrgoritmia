94ca7fe99a528f81bf6d55fef1fd2a84dca1666d
Keneth-Huerta <kenethissac@gmail.com>
2023-12-05 11:55:33 -0600

    cambios

--- src/Segundoalrgoritmia/Cuadradocubo.java
+++ src/Segundoalrgoritmia/Cuadradocubo.java
@@ -6,10 +6,10 @@ public class Cuadradocubo {
 
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    double num;
+    double num=0,  sucesivo=0;
 
     for (int i = 1; i <= 10; i++) {
-      double sucesivo = sucesivo * Math.pow(num, 2);
+       sucesivo = sucesivo * Math.pow(num, 2);
     }
   }
 }
