bba1b78774fae1fcc6b12adb9eff6c81f0be8f87
Keneth-Huerta <kenethissac@gmail.com>
2023-12-15 11:58:42 -0600

    TODO

---
+++ src/Segundoalrgoritmia/FrecuenciaNumerosAleatorios.java
@@ -0,0 +1,27 @@
+package Segundoalrgoritmia;
+
+
+public class FrecuenciaNumerosAleatorios {
+
+    public static void main(String[] args) {
+        // Crea un arreglo de 100 números aleatorios entre 1 y 10
+        int[] numerosAleatorios = new int[100];
+        for (int i = 0; i < numerosAleatorios.length; i++) {
+            numerosAleatorios[i] = (int) (Math.random() * 10) + 1;
+        }
+
+        // Inicializa un arreglo de 10 posiciones para almacenar las frecuencias
+        int[] frecuencias = new int[10];
+
+        // Recorre el arreglo de números aleatorios
+        for (int numero : numerosAleatorios) {
+            // Incrementa la frecuencia del número en el arreglo de frecuencias
+            frecuencias[numero - 1]++;
+        }
+
+        // Imprime las frecuencias
+        for (int i = 0; i < frecuencias.length; i++) {
+            System.out.println("La frecuencia del número " + (i + 1) + " es " + frecuencias[i]);
+        }
+    }
+}
